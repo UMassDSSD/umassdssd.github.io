036bc5a66018791deb87d2b4665a12d344c92ad0
Manchun Wong <womchun@gmail.com>
2014-02-18 23:01:57 -0500

    Added page.js
    “page.js” is the main entry of webpage

---
+++ scripts/page.js
@@ -0,0 +1,38 @@
+(function(window) {
+var document = window.document;
+var location = window.location;
+var navigator = window.navigator;
+
+function makeUnderConstructionSection() {
+	var undercon = document.createElement("section");
+	undercon.id = "underconstruction";
+	undercon.appendChild((function(h1) {
+		h1.innerHTML += "CAUTION: <nobr>Under Construction</nobr>";
+		return h1;
+	})(document.createElement("h1")));
+	undercon.appendChild((function(div) {
+		div.innerHTML += "UMass Dartmouth Society for Software Developers";
+		return div;
+	})(document.createElement("div")));
+	undercon.appendChild((function(ul) {
+		ul.innerHTML += "<li><a href='//facebook.com/groups/566724943376702' target='_blank'>Facebook</a></li>";
+		ul.innerHTML += "<li><a href='//github.com/UMassDSSD' target='_blank'>GitHub</a></li>";
+		return ul;
+	})(document.createElement("ul")));
+	undercon.appendChild((function(div) {
+		div.classList.add("hint");
+		div.innerHTML += "A <pre>?dev=yes</pre> may help.";
+		return div;
+	})(document.createElement("div")));
+	return undercon;
+}
+
+var wrapper = document.createElement("div");
+wrapper.id = "wrapper";
+
+function onPageLoad() {
+	document.body.appendChild(Util.isDeveloperMode() ? wrapper : makeUnderConstructionSection());
+}
+
+window.addEventListener("load", onPageLoad, false);
+})(window);
