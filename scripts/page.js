300dbff7d1f6fa7d80ca33432d0a547b6716268f
Manchun Wong <womchun@gmail.com>
2014-02-21 05:11:48 -0500

    Outline of the wrapper

--- scripts/page.js
+++ scripts/page.js
@@ -12,6 +12,15 @@ var element = Util.DOM.Element;
 var ajax = Util.AJAX.Get;
 var getGET =  Util.URL.getGET;
 
+var wrapper = element.div().id("wrapper").child([
+	element.header().id("page-header").child([
+		element.a().id("logo").child("UMDSSD").attributes({
+			"href": ".?p=" + homePage
+		}),
+		element.nav().id("navbar").child(element.ul())
+	]),
+	element.section().id("page-content")
+]);
 
 
 function onPageLoad() {
