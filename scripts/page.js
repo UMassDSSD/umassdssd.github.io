d5dfb1828a58f77f6b9769b5c6c4ff25abed20de
Manchun Wong <womchun@gmail.com>
2014-02-21 04:08:15 -0500

    Over construction

--- scripts/page.js
+++ scripts/page.js
@@ -7,26 +7,6 @@ var navigator = window.navigator;
 //Shortcut to Util.DOM.Element
 var element = Util.DOM.Element;
 
-function makeUnderConstructionSection() {
-	return element.section().id("underconstruction").child([
-		element.h1().child("CAUTION: <nobr>Under Construction</nobr>"),
-		element.div().child("UMass Dartmouth Society for Software Developers"),
-		element.ul().child([
-			element.li().child(element.a().attributes({
-				"href": "//facebook.com/groups/566724943376702",
-				"target": "_blank"
-			}).child("Facebook")),
-			element.li().child(element.a().attributes({
-				"href": "//github.com/UMassDSSD",
-				"target": "_blank"
-			}).child("GitHub"))
-		]),
-		element.div().classList("hint")
-			.child("A ")
-			.child(element.pre().child("?dev=yes"))
-			.child(" may help.")
-	]);
-}
 
 var wrapper = element.div().id("wrapper").child(
 	element.pre()
