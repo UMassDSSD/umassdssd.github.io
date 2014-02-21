92093a501d607e55be0441281c850b40f10671b2
Manchun Wong <womchun@gmail.com>
2014-02-21 04:08:42 -0500

    We are already working on it.
    But still, feel free to contribute though.

--- scripts/page.js
+++ scripts/page.js
@@ -8,15 +8,6 @@ var navigator = window.navigator;
 var element = Util.DOM.Element;
 
 
-var wrapper = element.div().id("wrapper").child(
-	element.pre()
-		.child("Yes! You could be the developer! Feel free to push something to the ")
-		.child(element.a().attributes({
-			"href": "//github.com/UMassDSSD/umassdssd.github.io",
-			"target": "_blank"
-		}).child("repo"))
-		.child(" :-)")
-);
 
 function onPageLoad() {
 	element(document.body).child(Util.isDeveloperMode() ? wrapper : makeUnderConstructionSection());
